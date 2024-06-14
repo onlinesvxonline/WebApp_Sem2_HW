c58eff371dbf70f4dc09af26a1c23c9e4b7b3aaf
Denis Chornonog <onlinesvxonline@mail.ru>
2024-06-14 16:34:01 +0300

    Добавьте файлы проекта.

---
+++ WebAppGeek/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using WebAppGeek.Abstraction;
+using WebAppGeek.Dto;
+using WebAppGeek.Models;
+
+namespace WebAppGeek.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet("download_csv")]
+        public IActionResult DownloadCsvFile()
+        {
+            var products = _productRepository.GetAllProducts();
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Name,Price,Description,ProductGroupId");
+
+            foreach (var product in products)
+            {
+                sb.AppendLine($"{product.Name},{product.Price},{product.Description},{product.ProductGroupId}");
+            }
+
+            byte[] bytes = Encoding.UTF8.GetBytes(sb.ToString());
+            return File(bytes, "text/csv", "products.csv");
+        }
+    }
+}
---
+++ WebAppGeek/Controllers/Statistics.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebAppGeek.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CacheStatisticsController : ControllerBase
+    {
+        [HttpGet("cache_statistics")]
+        public IActionResult GetCacheStatistics()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "statistics.txt");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "text/plain", "statistics.txt");
+        }
+    }
+}
